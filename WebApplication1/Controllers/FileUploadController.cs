619e1ce9979a5c10d6801c882523a4c2336869d8
chuangmaster <masterchuang@urtody.com>
2018-04-20 18:02:27 +0800

    all

---
+++ WebApplication1/Controllers/FileUploadController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class FileUploadController : Controller
+    {
+        // GET: FileUpload
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Post(UploadFileModel photos)
+        {
+            var success = false;
+            var directory = @"D:\";
+            if (!ModelState.IsValid)
+            {
+                var errorMsg = new StringBuilder();
+                ModelState.Values.ToList().ForEach(x =>
+                {
+                    x.Errors.ToList().ForEach(y => { errorMsg.AppendLine(y.ErrorMessage); });
+                });
+                foreach (var item in ModelState.Values)
+                {
+
+                }
+
+                return Content("<h1>error</h1>");
+            }
+
+            //if (photos.File != null && photos.File.ContentLength > 0)
+            //{
+            //    var fileName = Path.GetFileName(photo.FileName);
+            //    photo.SaveAs(Path.Combine(directory, fileName));
+            //    success = true;
+            //}
+
+            return Content("<h1>upload success!</h1>");
+
+        }
+
+
+        //public ActionResult Post(IEnumerable<HttpPostedFileBase> photos)
+        //{
+        //    var success = false;
+        //    var directory = @"D:\";
+        //    //HttpPostedFileBase photo = Request.Files["photo"];
+        //    var errorMsg = new StringBuilder();
+        //    foreach (var photo in photos)
+        //    {
+        //        if (photo == null)
+        //        {
+        //            errorMsg.AppendLine("No files were uploaded ");
+        //            break;
+        //        }
+        //        errorMsg.Clear();
+        //        if (photo.ContentLength > 10240)
+        //        {
+        //            errorMsg.AppendLine("Photo should not be execeed 10KB ");
+        //            break;
+        //        }
+        //        var extensionName = Path.GetExtension(photo.FileName).Substring(1);
+        //        var supportedTypes = new[] { "jpg", "jpeg", "png" };
+
+        //        if (!supportedTypes.Contains(extensionName))
+        //        {
+        //            errorMsg.AppendLine("Not support this file type");
+        //            break;
+        //        }
+
+        //        if (photo != null && photo.ContentLength > 0)
+        //        {
+        //            var fileName = Path.GetFileName(photo.FileName);
+        //            photo.SaveAs(Path.Combine(directory, fileName));
+        //            success = true;
+        //        }
+
+
+        //    }
+
+        //    return success ? Content("<h1>upload success!</h1>") : Content($"<h1>{errorMsg}</h1>");
+
+        //}
+    }
+}
---
+++ WebApplication1/Models/UploadFileModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models.Attribute;
+
+namespace WebApplication1.Models
+{
+    public class UploadFileModel
+    {
+        [FileSize(10240)]
+        [FileTypes("jpg,jpeg,png")]
+        public string File { get; set; }
+    }
+}
